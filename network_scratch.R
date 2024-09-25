6bb054991b86086a2cbd874a4458f07969fbf321
Benitez <lb37426@my.utexas.edu>
2024-09-25 17:34:31 -0500

    added some scratch code etc.

---
+++ network_scratch.R
@@ -0,0 +1,8 @@
+##### playing around with SI networks
+load("C:/Users/lb37426/Dropbox/skyIslands/data/networks/Yr_PlantPollinator_Bees.RData")
+plotweb(nets[["CH.2017"]], col.high = "lightblue", col.low = "darkgreen", text.rot=90, labsize
+=0.7)
+plotweb(nets[["CH.2018"]], col.high = "lightblue", col.low = "darkgreen", text.rot=90, labsize
+        =0.7)
+plotweb(nets[["CH.2021"]], col.high = "lightblue", col.low = "darkgreen", text.rot=90, labsize
+        =0.7)
